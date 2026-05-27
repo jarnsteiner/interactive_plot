075ed54035eeb8f6031af07bd64649e9887dc2f8
Laurence Bichlbauer <lth.bichlbauer04@mci4me.at>
2026-05-27 19:23:50 +0200

    Abgabe: Interaktiver Plot

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src import load_data
-from src import zones
+from src import zones_data
 import plotly.graph_objects as go
-
+import streamlit as st
 
 
 
@@ -9,22 +9,84 @@ def main():
     
     df = load_data.load_activity("data/activity.csv")
     #print(df)
-    mittelwert, maximum = load_data.get_stats(df)
-    
-    print(f"Mittelwert: {mittelwert}, Maximum: {maximum}")
+    W_mid, W_max = load_data.get_stats(df)
 
+    st.title("Herzfrequenz")
+
+    max_hr = user_input()
+    #print(max_hr)
+    #print(f"Mittelwert: {W_mid}, Maximum: {W_max}")
+
+    
     #zones.plot_zones(data)
     # x=[1, 2, 3, 4]
     # y=[10, 15, 8, 20]
+    
     fig = go.Figure()
+    fig2 = go.Figure()
     fig.add_trace(
         go.Scatter(
             x = df["Time"],
             y = df["HeartRate"],
-            mode="lines"
+            mode="lines",
+            line=dict(smoothing = 1.3, color="red"),
+            line_shape="spline",
+            name= "Heartrate"
             )
-        )  
-    fig.show()
+        )
+    
+    fig2.add_trace(
+        go.Scatter(
+            x = df["Time"],
+            y = df["PowerOriginal"],
+            mode="lines",
+            line=dict(smoothing = 1.3, color="blue"),
+            line_shape="spline",
+            name= "PowerOrigin"
+            )
+        ) 
+    fig.update_layout(
+        width=800,
+        height=500,
+        plot_bgcolor="black",
+        paper_bgcolor="black",
+        font=dict(color="white")
+    )
+    fig2.update_layout(
+        width=800,
+        height=500,
+        plot_bgcolor="black",
+        paper_bgcolor="black",
+        font=dict(color="white")
+    )
+    zones =zones_data.zones_rect(max_hr)
+
+    for lower, upper, color in zones:
+        fig.add_hrect(
+            y0=lower,
+            y1=upper,
+            fillcolor=color,
+            opacity=0.25,
+            line_width=0
+            )
+
+
+    st.plotly_chart(fig)
+    st.title("Power")
+    st.plotly_chart(fig2)
+    st.title("Zeiten in den Zonen")
+    st.table(zones_data.zone_summary(df, max_hr))
+
+def user_input():
+
+    max_hr = st.number_input(
+        "Bitte geben sie Ihre Maximale Herzfrequenz ein:",
+        min_value=120,
+        max_value=270,
+        value=200
+    )
+
+    return max_hr
 
 
 if __name__ == "__main__":
---
+++ src/zones_data.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+def zones_rect(max_hr):
+
+    zone = [
+    (0.5 * max_hr, 0.6 * max_hr, "lightgreen"),
+    (0.6 * max_hr, 0.7 * max_hr, "green"),
+    (0.7 * max_hr, 0.8 * max_hr, "yellow"),
+    (0.8 * max_hr, 0.9 * max_hr, "orange"),
+    (0.9 * max_hr, 1.0 * max_hr, "red")
+    ]
+
+    return zone 
+
+def zone_summary(df, max_hr):
+    grenzen = [0, 0.60, 0.70, 0.80, 0.90, 1.01]
+
+    data = df.copy()
+
+    data["zone"] = pd.cut(
+        data["HeartRate"],
+        bins = [i * max_hr for i in grenzen],
+        labels = [1, 2, 3, 4, 5]
+
+    ).astype("Int64")
+
+
+
+    summary = data.groupby("zone").agg({
+        "PowerOriginal": "mean",
+        "zone": "count"
+    })
+
+    summary.columns = ["Power", "Zeit"]
+
+    return summary
+
+# test_data = pd.DataFrame({
+#     "heart_rate": [100, 120, 140, 160, 180, 190],
+#     "power": [90, 110, 150, 200, 220, 250]
+# })
+
+# data = zones(test_data, 190)
+
+# print(data)
