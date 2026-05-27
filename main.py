cc83a37d4be1d8dea053502a855a655a80e9a1c2
Laurence Bichlbauer <lth.bichlbauer04@mci4me.at>
2026-05-27 17:52:36 +0200

    main und plot

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
 from src import load_data
 from src import zones
+import plotly.graph_objects as go
+
+
+
 
 def main():
+    
     data = load_data.load_activity("data/activity.csv")
+
     mittelwert, maximum = load_data.get_stats(data)
+    
     print(f"Mittelwert: {mittelwert}, Maximum: {maximum}")
 
     #zones.plot_zones(data)
+    x=[1, 2, 3, 4]
+    y=[10, 15, 8, 20]
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+            x = x,
+            y = y,
+            mode="lines"
+            )
+        )  
+    fig.show()
+
+
+
+
+
+
+
+
+
+
+     
+
+
+
+
+
+
 
 if __name__ == "__main__":
-    main()
+    main() 
